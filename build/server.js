1904080ef08a21e2e9ca91db2b09c0389df0fd89
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-01-27 01:44:17 +0200

    main

--- build/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app = require("./index");
-var PORT = process.env.PORT;
-app.listen(PORT, function () {
-    console.log("Heolooooolo sunuvan bitch server listen on port ".concat(PORT));
-});
