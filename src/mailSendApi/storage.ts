6afbdbe94dda4d3fc00da0bc4de3acf0c3470465
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-04-24 21:32:41 +0300

    main

--- src/mailSendApi/storage.ts
+++ src/mailSendApi/storage.ts
@@ -3,7 +3,7 @@ import uniqid from "uniqid";
 const multer = require("multer");
 const path = require("path");
 
-const absolutePath = path.resolve("uploads");
+const absolutePath = path.resolve("./uploads");
 
 const storage = multer.diskStorage({
   destination: (req: any, file: any, cb: any) => {
